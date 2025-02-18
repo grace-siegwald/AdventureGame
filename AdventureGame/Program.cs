e486fa40014f31d97a91499cf77ab82d28c355d2
grace-siegwald <Siegwald@colum.edu>
2025-02-18 16:02:10 -0600

    Add project files.

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventureGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            World world = new World();
+            world.Setup();
+        }
+    }
+}
---
+++ AdventureGame/World.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    internal class World
+    {
+        //set of locations
+        //player
+        
+        //method that will start up the world
+        public void Setup()
+        {
+            
+        }
+    }
+}
